f9170acef636bb15230a920de53f5b519364894c
scoty755 <scoty755@gmail.com>
2018-05-03 21:16:20 +0900

    msm8974-common: Move init.carrier.rc to /vendor/etc/init/hw

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -49,7 +49,7 @@ LOCAL_MODULE       := init.carrier.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.carrier.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 else
 include $(CLEAR_VARS)
